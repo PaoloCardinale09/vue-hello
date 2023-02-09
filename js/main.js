796f9a4164acbcad2fc0ed161daf3b9fc28bb355
PaoloCardinale09 <alfiozeronove@gmail.com>
2023-02-09 17:45:27 +0100

    prova aggiunta funzione

--- js/main.js
+++ js/main.js
@@ -9,12 +9,24 @@
 const app = Vue.createApp({
   data() {
     return {
-      saluto: "ciao",
+      saluto: { cordiale: "buongiorno", normale: "salve", arrabiato: "vaff" },
       nome: "Paolo",
       cognome: "Cardinale",
       immagine:
         "https://www.esa.int/var/esa/storage/images/esa_multimedia/images/2022/03/the_sun_in_high_resolution/24010613-1-eng-GB/The_Sun_in_high_resolution_pillars.jpg",
     };
+  },
+
+  methods: {
+    saluta() {
+      let time = "giorno";
+      if (time == "giorno") {
+        return this.saluto.cordiale;
+      } else if (time == "sera") {
+        return this.saluto.arrabiato;
+      }
+      return this.saluto.normale;
+    },
   },
 });
 
